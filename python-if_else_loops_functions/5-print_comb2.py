3e76dfe410b1ed406b2eb84d32a17ef55c6e3c74
Tyler W <tyler.whitchurch@atlasschool.com>
2024-09-02 08:34:02 -0500

    changed starting number to 0

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(1, 101):
+for i in range(0, 101):
     if i < 100:
         print("{:02d}".format(i), end=", ")
     else:
         print("{:02d}".format(i))
-        
