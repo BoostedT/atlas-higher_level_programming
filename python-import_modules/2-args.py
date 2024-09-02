9ce79730015fd10e7ddcbb9fe21563c02407db60
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-02 10:02:05 -0500

    fixed code to count total arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def print_arguments(*argv):
-    num_args = len(argv)
-    print(f"Number of argument{'s' if num_args != 1 else ''}:")
-    if num_args == 0:
-        print("(or . if no arguments were passed)")
-    else:
-        for i, arg in enumerate(argv, start=1):
-            print(f"{i}:", arg)
-
 if __name__ == "__main__":
     import sys
-    print_arguments(*sys.argv[1:])
+    argLen = len(sys.argv)
+    if argLen == 1:
+        print("{} arguments.".format(argLen - 1))
+    elif argLen == 2:
+        print("{} argument:".format(argLen - 1))
+    else:
+        print("{} arguments:".format(argLen - 1))
+    for i in range(1, argLen):
+        print("{}: {}".format(i, sys.argv[i]))
+        
