c8d6d6de2343b3e278263d2f8740c9684fbeaa96
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-02 10:14:43 -0500

    fixed pycode style

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_arguments(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+
+if __name__ == "__main__":
+    import sys
+    arguments = list(map(int, sys.argv[1:]))
+    result = add_arguments(*arguments)
+    print("The result of the addition is:", result)
