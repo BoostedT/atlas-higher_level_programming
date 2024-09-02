44a08bd520d457dda9cc8506292de48503a497ab
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-02 18:59:16 -0500

    added switch statement

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-def switch(my_list=[]):
-    if len(my_list) == 0:
-        return (None)
-
-    new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            new_list.append(False)
-        else:
-            new_list.append(True)
-
-    return (new_list)
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
