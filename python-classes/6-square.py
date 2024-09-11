249a5da8ce30ec3d9d2e1e08e0159e838ebe7473
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-10 22:37:53 -0500

    fixed

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,7 +10,7 @@ class Square:
     class square
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0 , position=(0, 0)):
         """
         init method
         Args:
@@ -37,7 +37,7 @@ class Square:
     @property
     def position(self):
         """get position of square"""
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
