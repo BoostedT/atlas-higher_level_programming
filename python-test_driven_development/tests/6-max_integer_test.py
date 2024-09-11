779a9a32ac6499933a4d8cd0d1ba41a4590c86f0
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 18:18:38 -0500

    im excited to move forward

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer function
+    """
+
+    def test_max_integer(self):
+        """Test max_integer function
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([1, 2, 3, 4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([4, 3, 2, 1, 2, 3, 4]), 4)
+
+        def test_type_error(self):
+            """type error test
+            """
+            self.assertRaises(TypeError, max_integer, [1, 2, "hello"])
+            self.assertRaises(TypeError, max_integer, [1, [1, 2]])
+            
