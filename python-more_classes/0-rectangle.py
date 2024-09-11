7f5c23bd21acb3d7676b664d96df6bbed24a2b20
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 18:54:10 -0500

    added 0-rectangle.py and README.md

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contain a class that defines a rectangle"""
+
+class Rectangle:
+    """This class defines a rectangle"""
+
+    def __init__(self, length, width):
+        """Initialize a Rectangle object.
+
+        Parameters:
+        length (int, float): The length of the rectangle.
+        width (int, float): The width of the rectangle.
+        """
+        self.length = length
+        self.width = width
