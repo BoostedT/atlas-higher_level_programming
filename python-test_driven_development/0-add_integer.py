09060d0890dc4436f1b5465b31d45fd07f6e2827
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-10 23:30:33 -0500

    added documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,14 @@ module 0-add_integer
 """
 
 def add_integer(a, b=98):
+    """
+    adds two integers
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        the addition of the two integers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
