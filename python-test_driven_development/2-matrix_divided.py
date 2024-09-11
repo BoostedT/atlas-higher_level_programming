ade0e1cc07f07385a8e51155694619ea36cd1eb4
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 00:02:59 -0500

    yes

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,7 +2,7 @@
 """
 Module: 2-matrix_divided
 
-Divides all elements of a matrix by
+Divides all elements of a matrix by 
 a given number.
 """
 
@@ -16,20 +16,20 @@ def matrix_divided(matrix, div):
         div (int or float): The number to divide the matrix elements by.
     """
     if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
-        raise TypeError("matrix must be a matrix of integers/floats")
+        raise TypeError("matrix must be a matrix of (list of lists) integers/floats")
 
     for row in matrix:
         if len(row) == 0:
-            raise TypeError("matrix must be a matrix of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         for x in row:
             if not isinstance(x, (int, float)):
-                raise TypeError("matrix must be a matrix of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_size = len(matrix[0])
     for row in matrix:
         if len(row) != row_size:
             raise TypeError("Each row of the matrix must have the same size")
-
+        
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
