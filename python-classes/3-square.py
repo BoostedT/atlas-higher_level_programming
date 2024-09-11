41e8645c4ac7cbab16bd559148df396b731b366a
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-10 22:04:17 -0500

    y

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,12 +11,20 @@ class Square:
     """
     def __init__(self, size=0):
         """
-        This is a method that initializes the square
+        Initialize a Square object.
+
+        Parameters:
+        size (int, optional): The side length of the square. Defaults to 0.
+
+        Raises:
+        TypeError: If the size is not an integer.
+        ValueError: If the size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
 
     def area(self):
         """
