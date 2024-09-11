7e22b6524d61619d49fd43b25ed3ef4c8c1f16b1
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-10 22:35:11 -0500

    ok

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,11 +52,13 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Print the square with #"""
         if self.__size == 0:
-            print()
+            print("")
             return
-        for i in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
