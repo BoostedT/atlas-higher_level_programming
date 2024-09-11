97e8ab6b572cd5c9630f3437474f0ab119eb2495
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 11:09:25 -0500

    add module

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""Module that prints a square with the character #."""
+
 def print_square(size):
     """Prints a square with the character #."""
     if not isinstance(size, int):
