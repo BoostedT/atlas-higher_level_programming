e7edb966a733e09aec1926651ae904ce7a64545e
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 18:35:55 -0500

    made one method for tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,26 +8,27 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Test cases for max_integer function
     """
-
-    def test_regular(self):
-        """Test regular numbers
+    def test_max_integers(self):
+        """Test max_integer with integers
         """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
-    def test_not_list(self):
-        """Test when the argument is not a list
-        """
-        with self.assertRaises(TypeError):
-            max_integer(1)
-
-    def negative_test(self):
-        """Test negative numbers
-        """
+        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([1, 1, 1, 2]), 2)
+        self.assertEqual(max_integer([1, 1, 2, 1]), 2)
+        self.assertEqual(max_integer([1, 2, 1, 1]), 2)
+        self.assertEqual(max_integer([2, 1, 1, 1]), 2)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
         self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
-
+        self.assertEqual(max_integer([-1, -4, -3, -2]), -1)
+        self.assertEqual(max_integer([-1]), -1)
+        self.assertEqual(max_integer([-1, -1, -1, -1]), -1)
+        self.assertEqual(max_integer([-1, -1, -1, -2]), -1)
+        self.assertEqual(max_integer([-1, -1, -2, -1]), -1)
+        self.assertEqual(max_integer([-1, -2, -1, -1]), -1)
+        self.assertEqual(max_integer([-2, -1, -1, -1]), -1)
 
 if __name__ == '__main__':
     unittest.main()
