1a16267047d5b601ba94974fed99aa76a76d76b6
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 18:59:47 -0500

    removed extra addons

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -3,13 +3,4 @@
 
 class Rectangle:
     """This class defines a rectangle"""
-
-    def __init__(self, length, width):
-        """Initialize a Rectangle object.
-
-        Parameters:
-        length (int, float): The length of the rectangle.
-        width (int, float): The width of the rectangle.
-        """
-        self.length = length
-        self.width = width
+    pass
