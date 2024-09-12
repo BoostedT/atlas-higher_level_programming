e2a8e6730769b5e18d0cfed6d6ebbd8fe4ff5f6d
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 19:02:37 -0500

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines a rectangle"""
+
+class Rectangle:
+    """Class Rectangle that defines a rectangle"""
+
+    def __init__(self, width, height):
+        """
+        Initializes the rectangle with given width and height.
+        """
+        self.width = width
+        self.height = height
