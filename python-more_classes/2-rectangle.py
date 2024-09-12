bbd555f6edff49da05a73c14b98a00f1aeb584e9
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 19:23:52 -0500

    pycodestyle

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a rectangle"""
 
+
 class Rectangle:
     """Class Rectangle that defines a rectangle"""
 
@@ -15,11 +16,11 @@ class Rectangle:
     def width(self):
         """Returns the width of the rectangle"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Sets the width of the rectangle"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -29,11 +30,11 @@ class Rectangle:
     def height(self):
         """Returns the height of the rectangle"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Sets the height of the rectangle"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -42,7 +43,7 @@ class Rectangle:
     def area(self):
         """Returns the area of the rectangle"""
         return self.width * self.height
-    
+
     def perimeter(self):
         """Returns the perimeter of the rectangle"""
         if self.width == 0 or self.height == 0:
