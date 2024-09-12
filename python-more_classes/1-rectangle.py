0d9cf62fb6e0f9b3c8cca6a30302aed376bca157
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-11 19:07:07 -0500

    added 1-rectangle.py and defined a class Rectangle that defines a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,3 +10,31 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+
+    @property
+    def width(self):
+        """Returns the width of the rectangle"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the rectangle"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
