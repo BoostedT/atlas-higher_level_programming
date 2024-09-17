4e7c4ca6296c0f814e07dab1d894576f75bb379e
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 00:58:19 -0500

    fixed spacing

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """Module that defines a custom list class with additional methods."""
 
+
 class MyList(list):
     """Custom list class with additional methods.
     """
     def __init__(self):
         super().__init__()
 
+
 def print_sorted(self):
     """Prints the list, in ascending order by value."""
     print(sorted(self))
+
 
 if __name__ == "__main__":
     import doctest
