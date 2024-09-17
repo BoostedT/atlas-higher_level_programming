2fe6a76a48cd9dc8532ca4e06efc9b1ee5e5f19b
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 00:21:26 -0500

    added is instance and more pep8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ Prints the current working directory and its contents. """
 
+
 def is_same_class(obj, a_class):
     """Check if an object is exactly an instance of a specified class.
-    
+
     Args:
         obj: The object to check.
         a_class: The class to compare against.
-    
+
     Returns:
         True if obj is an instance of a_class; False otherwise.
     """
-    if type(obj) == a_class:
+    if isinstance(obj) == a_class:
         return True
     else:
         return False
