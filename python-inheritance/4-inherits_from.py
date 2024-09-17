4980256d2d5f8745f3f38455fae24611351ac840
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 01:12:00 -0500

    added pep8 fixes

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,7 @@ This module contains a function that checks if an object is an instance
 of a class that inherited (directly or indirectly) from the specified class.
 """
 
+
 def inherits_from(obj, a_class):
     """Checks if an object is an instance of a class that inherited from
     a_class.
