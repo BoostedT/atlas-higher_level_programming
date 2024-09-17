f270254cd1f6fefef8e14cdfeb6c54c838c9277f
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 00:00:32 -0500

    PEP8

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a function to lookup attributes 
+"""Module that defines a function to lookup attributes
 and methods of an object."""
+
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object."""
