908cb4ba6b4e221a236e89d8b568576781bb031e
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 23:27:30 -0500

    pep 8

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" writes a string to a text file and returns the number of characters written """
+""" writes a string to a text file and 
+returns the number of characters written """
 
 
 def write_file(filename="", text=""):
-    """ writes a string to a text file and returns the number of characters written """
+    """ writes a string to a text file and 
+    returns the number of characters written """
 
     with open(filename, 'w') as f:
         return f.write(text)
