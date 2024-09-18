e77ab6795a732e39d1a4e993ee37a9deb091a48d
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 23:14:31 -0500

    too many spaces fixed

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,18 +8,15 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ This class defines a Square that inherits from Rectangle. """
 
-
     def __init__(self, size):
         """ Initializes the size of the square. """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
-
     def area(self):
         """ Returns the area of the square. """
         return self.__size ** 2
-
 
     def __str__(self):
         """ Returns a string representation of the square. """
