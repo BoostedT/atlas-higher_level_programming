6d3b178311d159d7703c52a7c04e792fe4a1cafd
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 23:50:42 -0500

    oops i had typed wrong number in

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ import json
 
 argv = sys.argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
