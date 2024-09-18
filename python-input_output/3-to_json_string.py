951b90666cef767887f2637201eb50844d12e3b1
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 23:34:05 -0500

    pep

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" returns the JSON representation of an object (string) """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object (string) """
+
+    return json.dumps(my_obj)  # Convert the object to a JSON string
