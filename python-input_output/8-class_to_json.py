ef3494b225f2c9da281ee0e40989c56b295d8d32
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 23:52:29 -0500

    removed import

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,8 +2,6 @@
 """ function that returns the dictionary description
 with simple data structure for JSON serialization of an object """
 
-import json
-
 def dict_description(obj):
     """Function that returns the dictionary description
     with simple data structure for JSON serialization of an object
