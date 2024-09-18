07556bd020c9a907c61aed056282e90fb1cb92a0
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 23:53:27 -0500

    1

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,11 +2,8 @@
 """ function that returns the dictionary description
 with simple data structure for JSON serialization of an object """
 
-def dict_description(obj):
-    """Function that returns the dictionary description
-    with simple data structure for JSON serialization of an object
+def class_to_json(obj):
+    """ returns the dictionary description with simple data structure
+    for JSON serialization of an object """
 
-    Args:
-        obj (object): object to be serialized
-    """
     return obj.__dict__
