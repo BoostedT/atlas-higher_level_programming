474ed9d2e9b03e6689ac1055fa5028fd26ef7d4a
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 22:56:44 -0500

    pep8

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,7 +5,6 @@
 class BaseGeometry:
     """ a class BaseGeometry (based on 5-base_geometry.py). """
 
-
     def area(self):
         """ raises an Exception with the message area() is not implemented """
         raise Exception("area() is not implemented")
