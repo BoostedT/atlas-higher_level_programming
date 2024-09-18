0db1583efce964c79b1a5906f8a9556bf3876c88
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-17 23:14:04 -0500

    over indented fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,15 +9,15 @@ class Square(Rectangle):
     """ This class defines a Square that inherits from Rectangle. """
 
     def __init__(self, size):
-            """ Initializes the size of the square. """
-            self.integer_validator("size", size)
-            self.__size = size
-            super().__init__(size, size)
+        """ Initializes the size of the square. """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-            """ Returns the area of the square. """
-            return self.__size ** 2
+        """ Returns the area of the square. """
+        return self.__size ** 2
 
     def __str__(self):
-            """ Returns a string representation of the square. """
-            return "[Square] {}/{}".format(self.__size, self.__size)
+        """ Returns a string representation of the square. """
+        return "[Square] {}/{}".format(self.__size, self.__size)
