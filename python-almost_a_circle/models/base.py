32c23a28af30ca3aa1acea8cdd4f21e2d6d61b13
BoostedT <tyler.whitchurch@atlasschool.com>
2024-09-22 22:09:58 -0500

    created base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" module for base class """
+
+import os
+
+class Base:
+    """Base Class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """init method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return JSON string representation of list_dictionaries"""
+        import json
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        list_dicts = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dicts.append(obj.to_dictionary())
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return list of JSON string representation"""
+        import json
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """return instance with all attributes set"""
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        if cls.__name__ == "Square":
+            new = cls(1)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """return list of instances"""
+        filename = cls.__name__ + ".json"
+        if not os.path.exists(filename):
+            return []
+        with open(filename, "r") as file:
+            list_dicts = cls.from_json_string(file.read())
+        list_objs = []
+        for dict in list_dicts:
+            list_objs.append(cls.create(**dict))
+        return list_objs
