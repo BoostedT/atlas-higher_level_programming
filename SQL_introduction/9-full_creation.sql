effaacabb6eb6fe068edc06b6686e4709940d014
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-12 01:41:26 -0500

    changed it up

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,9 @@
 -- creates second table with multiple rows
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), age INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT, 
+    name VARCHAR(256), 
+    age INT
+);
 INSERT INTO second_table (id, name, age) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, age) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, age) VALUES (3, 'Bob', 14);
