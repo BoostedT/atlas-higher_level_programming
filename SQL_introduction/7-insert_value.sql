e92fe475cb7e70448d6c1251dc2f6cbd0c4360e1
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-12 01:28:29 -0500

    i just re wrote it

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- inserting a new row into the table
-use hbtn_0c_0;
-INSERT INTO `first_table` ('id', 'name') VALUES (89, "Best School");
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
