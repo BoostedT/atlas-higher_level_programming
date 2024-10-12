8332a57a28cef861b26450dd1c9c59cd89f3bcf5
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-12 01:44:20 -0500

    trying new things

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- creates second table with multiple rows
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT, 
-    name VARCHAR(256), 
-    score INT
+    'id' INT, 
+    'name' VARCHAR(256), 
+    'score' INT
 );
 INSERT INTO second_table (id, name, age) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, age) VALUES (2, 'Alex', 3);
