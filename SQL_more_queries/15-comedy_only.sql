8b87ed97a0d789b249ce2b31d1dfead979cd4020
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 20:52:32 -0500

    as

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 --  a script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT title FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE genres.name = 'Comedy'
 ORDER BY title ASC;
