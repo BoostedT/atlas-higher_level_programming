522dcf7e9478bac0e0794e5dd03ad1bf7a5d3ab3
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 19:16:01 -0500

    im unsure now

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,3 @@
 -- creates a table for mysql server
 
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-);
+CREATE TABLE IF force_name (id INT, name VARCHAR(256), NOT NULL);
