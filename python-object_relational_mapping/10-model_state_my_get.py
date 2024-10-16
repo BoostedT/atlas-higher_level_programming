c07879046c480d4f128d0e3f0cd1b631a4c1f739
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-16 00:15:08 -0500

    pesky ws

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a script that prints the State object 
-with the name passed as argument from the database 
+"""a script that prints the State object
+with the name passed as argument from the database
 hbtn_0e_6_usa"""
 
 from sys import argv
