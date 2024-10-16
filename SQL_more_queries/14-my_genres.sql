b168fbd9c3f2b63d75163bd5f4eaa219fdf5ab81
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 20:45:39 -0500

    genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Use the tv_shows, tv_show_genres, and genres tables to find the genres of the TV show Dexter. Your query should return the genre names in alphabetical order.
 SELECT name FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+JOIN genres ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY genres.name ASC;
