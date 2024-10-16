bcfb3cece2f1da85bfeef45b05787b14d16cfbf3
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 19:08:44 -0500

    kim jung

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- creates user 2 and database for it
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS user_0d_2_db;
+Grant all privileges on user_0d_2_db.* to 'user_0d_2'@'localhost';
 
 -- connects to user 2's database
 USE user_0d_2_db;
