160c0c11d69f4a0cb26245fd4e60b92fe7a3729f
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 23:51:04 -0500

    pep8

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" a script that takes in arguments and displays all values in 
-the states table of hbtn_0e_0_usa where name matches the argument. 
+""" a script that takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections! """
 
 if __name__ == "__main__":
