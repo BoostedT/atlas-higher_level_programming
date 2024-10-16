d1cc074164417f59cd9ad9a8d0d69353bb999ec6
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 19:11:07 -0500

    maybe this will be correct

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates user 2 and database for it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE IF NOT EXISTS user_0d_2_db;
-Grant all privileges on user_0d_2_db.* to 'user_0d_2'@'localhost';
+Grant SELECT ON hbtn_0d_2 .* TO 'user_0d_2'@'localhost';
 Flush privileges;
