820c97b1207bc02acc7e9a3179737e5f32d0125a
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 19:09:20 -0500

    qwer

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,4 @@
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS user_0d_2_db;
 Grant all privileges on user_0d_2_db.* to 'user_0d_2'@'localhost';
-
--- connects to user 2's database
-USE user_0d_2_db;
+Flush privileges;
