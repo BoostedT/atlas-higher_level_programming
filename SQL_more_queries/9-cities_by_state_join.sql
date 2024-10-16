ae440ce18c91be18d7db4c2a8620aa20c73ca438
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 19:52:20 -0500

    ordering by id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- CITIES BY STATE
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY states.name, cities.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY id;
