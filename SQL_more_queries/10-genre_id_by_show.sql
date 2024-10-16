fcff933a357d45c65a099e189f7c20fdad3718f2
BoostedT <tyler.whitchurch@atlasschool.com>
2024-10-15 19:57:33 -0500

    fixed syntax

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT DISTINCT shows.title, shows.genre_id ORDER BY ASC;
+SELECT DISTINCT shows.title, shows.genre_id ORDER BY tv_shows.title tv_shows.genre_id ASC;
