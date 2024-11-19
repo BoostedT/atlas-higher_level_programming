7e1b361a17c3716f5752c6d1fd82069f05abbe7e
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-18 18:35:01 -0600

    semi standard

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 
-function add(a, b) {
-    return a + b;
-  }
-  
-  const args = process.argv.slice(2);
-  const firstNum = parseInt(args[0], 10);
-  const secondNum = parseInt(args[1], 10);
-  
-  if (isNaN(firstNum) || isNaN(secondNum)) {
-    console.log('NaN');
-  } else {
-    console.log(add(firstNum, secondNum));
-  }
-  
+function add (a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const firstNum = parseInt(args[0], 10);
+const secondNum = parseInt(args[1], 10);
+
+if (isNaN(firstNum) || isNaN(secondNum)) {
+  console.log('NaN');
+} else {
+  console.log(add(firstNum, secondNum));
+}
