53bfee787f7de73c755a60672d074be14de6ed39
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-18 18:49:40 -0600

    123

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
 
-function add (a, b) {
+exports.add = function(a, b) {
   return a + b;
-}
-
-const args = process.argv.slice(2);
-const firstNum = parseInt(args[0], 10);
-const secondNum = parseInt(args[1], 10);
-
-if (isNaN(firstNum) || isNaN(secondNum)) {
-  console.log('NaN');
-} else {
-  console.log(add(firstNum, secondNum));
 }
