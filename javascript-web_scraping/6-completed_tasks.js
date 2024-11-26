ec11482c37d2bdb91f0ff290241848f42cc61f08
BoostedT <tyler.whitchurch@atlasschool.com>
2024-11-25 21:32:04 -0600

    added code for #6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,17 @@
-#!/
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+if (!apiUrl) {
+  console.error('Please provide an API URL as an argument.');
+  process.exit(1);
+}
+
+request(apiUrl, { json: true }, (error, response, body) => {
+    if (err) {
+        console.error('Error fetching data:', err);
+        return;
+    }
+});
